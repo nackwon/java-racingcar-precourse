ab4153cf9a0655d39fbb503b42246f3c96597bbc
nackwon <ahwk321@gmail.com>
2022-04-25 16:05:23 +0900

    test : 자동차 전진,멈춤 조건 검증

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.domain.Car;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class CarTest {
@@ -14,5 +16,27 @@ public class CarTest {
         assertThatThrownBy(() -> {
             new Car("").getName();
         }).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR]");
+    }
+
+    @Test
+    public void 자동차_전진_조건_검증() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    Car car = new Car("woodi");
+                    car.runRound();
+                    assertThat(car.getDistance()).isEqualTo(1);
+                },4
+        );
+    }
+
+    @Test
+    public void 자동차_멈춤_조건_검증() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    Car car = new Car("woodi");
+                    car.runRound();
+                    assertThat(car.getDistance()).isEqualTo(0);
+                },3
+        );
     }
 }
